c0fa854cd9a3a91de8f64caa3870a20576810d3a
Lafon Sylvain <slafon@manzalab.com>
2016-12-06 15:10:06 +0100

    modifications

---
+++ Splash/LaunchObb.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchObb : MonoBehaviour {
+	
+	[Header("Settings")]
+	public string GoogleKey;
+	public bool UseObb;
+	public int SceneToLoad = 1;
+
+
+	[Header("Components")]
+	public Material fadeMaterial;
+	public SmoothScale progress;
+	public TextMesh progressText;
+
+	public virtual IEnumerator Start ()
+	{
+		yield return LoadObb();
+		Application.LoadLevel(SceneToLoad);
+	}
+
+	protected Coroutine LoadObb()
+	{
+		return StartCoroutine(LoadObbEnum());
+	}
+
+	private IEnumerator LoadObbEnum()
+	{
+#if !LOL || !UNITY_EDITOR && UNITY_ANDROID && USE_OBB
+
+		if(UseObb)
+		{
+			GooglePlayDownloader.APPLICATION_PUBLIC_KEY = GoogleKey;
+
+			if (!GooglePlayDownloader.RunningOnAndroid())
+				yield break;
+		
+			string expPath = GooglePlayDownloader.GetExpansionFilePath();
+			if (expPath == null)
+				yield break;
+		
+			string mainOBBPath = GooglePlayDownloader.GetMainOBBPath(expPath);
+			if (mainOBBPath == null)
+			{
+				GooglePlayDownloader.FetchOBB();			
+				while (mainOBBPath == null)
+				{
+					yield return new WaitForEndOfFrame();
+					mainOBBPath = GooglePlayDownloader.GetMainOBBPath(expPath);
+				}
+			}
+
+			GooglePlayDownloader.DeleteOBBExceptCurrent(expPath);
+
+			string uri = "file://" + mainOBBPath;
+			Debug.Log("loading OBB : " + uri);
+
+			if(progress)
+				progress.SetTarget(0);
+			if (progressText)
+				progressText.text = "0%";
+
+			WWW www = WWW.LoadFromCacheOrDownload(uri, 0);
+			//while (www.progress < 1)
+			//{
+			//	if (progress)
+			//		progress.SetTarget(www.progress);
+			//	if (progressText)
+			//		progressText.text = (int)(www.progress * 100) + "%";
+			//	Debug.Log("OBB progress = " + www.progress);
+			//
+			//	yield return new WaitForEndOfFrame();
+			//}
+			//
+			//if (progress)
+			//	progress.SetTarget(1);
+			//if (progressText)
+			//	progressText.text = "100%";
+
+			yield return www;
+					
+			Debug.Log("downloading finished " + uri);
+			if (www.error != null)
+			{
+				Debug.Log("wwww error " + www.error);
+				yield break;
+			}
+		}
+#else
+		yield break;
+#endif
+	}
+}
---
+++ Splash/SmoothScale.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class SmoothScale : MonoBehaviour 
+{
+	public  float target;
+	private float current;
+
+	public float SpeedHigher;
+	public float SpeedLower;
+
+	public bool UsesDuration;
+	public float DurationHigher;
+	public float DurationLower;
+	public float DurationRange;
+
+	public bool ResetOnEnable = true;
+	public float enableValue = 1;
+
+	public bool x = true, y = true, z = true;
+	private bool reached;
+	public bool canInvoke;
+	public UnityEvent OnTargetReached;
+
+	void Awake () 
+	{
+		current = target;
+	}
+
+	void OnEnable()
+	{
+		if (ResetOnEnable)
+		{
+			SetTarget(enableValue);
+			ForceScale(enableValue);
+		}
+	}
+
+	public void ForceScale(float scale)
+	{
+		current = scale;
+
+		Vector3 v = new Vector3();
+		v.x = x ? this.current : 1;
+		v.y = y ? this.current : 1;
+		v.z = z ? this.current : 1;
+
+		this.transform.localScale = v;
+	}
+
+	public void SetTarget(float scale)
+	{
+		target = scale;
+	}
+
+	public void SetInvokable(bool canInvoke)
+	{
+		this.canInvoke = canInvoke;
+	}
+
+	void Update()
+	{
+		if (UsesDuration)
+		{
+			SpeedHigher = DurationRange / DurationHigher;
+			SpeedLower = DurationRange / DurationLower;
+		}
+
+		if (this.target > this.current)
+		{
+			this.current += SpeedHigher * Time.deltaTime;
+			if (this.current > this.target)
+			{
+				this.current = this.target;
+			}
+		}
+
+		else if (this.target < this.current)
+		{
+			this.current -= SpeedLower * Time.deltaTime;
+			if (this.current < this.target)
+			{
+				this.current = this.target;
+			}
+		}
+
+		if (this.current == this.target)
+		{
+			if (!reached && canInvoke)
+				OnTargetReached.Invoke();
+			reached = true;
+		}
+		else
+		{
+			reached = false;
+		}
+
+		Vector3 scale = new Vector3();
+		scale.x = x ? this.current : 1;
+		scale.y = y ? this.current : 1;
+		scale.z = z ? this.current : 1;
+
+		this.transform.localScale = scale;
+	}
+}
